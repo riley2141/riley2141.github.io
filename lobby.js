c472bd983df206605cfb8882ef50e7e0ed4ccc6c
Riley Morrison <riley2141@gmail.com>
2016-11-06 18:34:05 -0600

    Added gameManager listeners for player_ready, available, and quit

--- lobby.js
+++ lobby.js
@@ -2,5 +2,13 @@
 
 
 function testList(){
-	document.getElementById("playerList").innerHTML = "Players<br>Hello<br>test3<br>4<br>5<br>6<br>7<br>8<br>9<br>10";
+	document.getElementById("playerList").innerHTML = "Players<br>Hello<br>test3<br>4<br>5<br>6<br>7<br>8<br>9<br>10<br>";
+}
+
+
+function updatePlayerList(playerList){
+	for(var i = 0; i < playerList.length; i++)
+	{
+		document.getElementById("playerList").innerHTML = document.getElementById("playerList").innerHTML + playerList[i] + "<br>";
+	}
 }
