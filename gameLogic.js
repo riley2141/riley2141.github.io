d85fc71f56f8e702c0b525dd98e425b9aec66307
Riley Morrison <riley2141@gmail.com>
2016-12-04 18:12:50 -0600

    math round fix round 2

--- gameLogic.js
+++ gameLogic.js
@@ -6,11 +6,8 @@ var gameData;
 
 function assignRoles(){
 	players = gameManager.getPlayersInState(cast.receiver.games.PlayerState.PLAYING);
-	var numEvil = players.length / 3;
-	if(players.length % 3 != 0)
-	{
-		numEvil = numEvil + 1;
-	}
+	var numEvil = Math.ceil(players.length / 3);
+	
 	console.log("number of players being assigned roles: " + players.length);
 	console.log("number of evil players: " + numEvil);
 
